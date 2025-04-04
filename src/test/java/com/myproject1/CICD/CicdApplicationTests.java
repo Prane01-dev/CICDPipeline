5702b4367b8a58d1c7377feae34eaee5217a2342
Priyanka <rathodpriya532@gmail.com>
2025-04-04 20:32:43 +0530

    commiting in 1.0 branch

---
+++ src/test/java/com/myproject1/CICD/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject1.CICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
